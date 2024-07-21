13920f8b6ccb854f2641bd11a02efe577c18b52e
SaiPranithx <duggireddysaipranith@outlook.com>
2024-07-21 10:13:41 +0530

    first commit

---
+++ scripts.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function () {
+    // Simulate a delay to show the loading bar
+    setTimeout(function () {
+        window.location.href = 'https://www.youtube.com';
+    }, 5000); // Change the delay time as needed
+});
